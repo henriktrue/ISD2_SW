792bff2a7c051c5733366751d928afd31c5614e4
Henrik Truelsen <31506722+henriktrue@users.noreply.github.com>
2018-04-07 20:53:27 +0200

    DD
    ASD

--- opgave4/klient/main.cpp
+++ opgave4/klient/main.cpp
@@ -1,17 +1,26 @@
-/* 
- * File:   main.cpp
- * Author: Henrik
- */
-
-
 #include <iostream>
 #include <signal.h>
 #include "connect.h"
-#include "control.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <signal.h>
+#include <syslog.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <pthread.h>
+#include <stdlib.h>
+#include <fstream>
 
 using namespace std;
 
-connect tcp;
+TCPClient tcp;
 int target = 25; //default target temperature
 float temp = 26; //default temperature reading resulting in "OFF" state
 
@@ -51,7 +60,7 @@ int main(int argc, char *argv[]) {
     if (argc == 2 && string(argv[1]).length() == 2) {
         target = toint(string(argv[1]));
     } else {
-        print_error();
+        print_help();
         exit(0);
     }
     signal(SIGINT, sig_exit);
@@ -59,15 +68,15 @@ int main(int argc, char *argv[]) {
 	// ip is 0.0.0.0 to listen on all interfaces
     tcp.setup("0.0.0.0", 1955); // ip and port
     while (1) {
-        tcp.Send("GETTEMP"); // ask host for temperature 
+        tcp.Send("GETTEMP"); // ask host for temperature
         string rec = tcp.receive(); // calls recv() and does error handling
-        
+
         if (rec != "") { // If rec is empty, recieve again
             cout << "Temperature is: " << rec << " degC" << endl;
-            
+
             // Convert temperature string to float, so i can compared to the setpoint in heater()
-            temp = stof(rec);
-            
+            float temp = strtof((rec).c_str(),0);
+
             // Instructs the host to turn heater on or off
             tcp.Send(tostr(heater(temp, target)));
         }
@@ -75,3 +84,5 @@ int main(int argc, char *argv[]) {
     }
     return 0;
 }
+
+
