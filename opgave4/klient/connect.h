b5a58519943c37533aa7177364768442fcc9bdc9
Henrik Truelsen <31506722+henriktrue@users.noreply.github.com>
2018-04-07 20:53:08 +0200

    AS
    A

--- opgave4/klient/connect.h
+++ opgave4/klient/connect.h
@@ -1,32 +1,37 @@
-/* 
- * File:   controller.h
- * Author: Henrik
- *
- */
+#ifndef CONNECT_H
+#define CONNECT_H
 
-#ifndef CONTROL_H
-#define CONTROL_H
-
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <syslog.h>
-#include <sstream>
-#include <pthread.h>
-#include <stdlib.h>
-#include <fstream>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <netdb.h>
-#include <iostream>
-#include <string>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <string.h>
-#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netdb.h>
+#include <vector>
+#include <cstddef>
 
-void print_help(void);
-int toint(std::string s);
-int heater(float temperature, int target);
+using namespace std;
 
-#endif /* CONTROL_H */
+class TCPClient
+{
+  private:
+    int sock;
+    std::string address;
+    int port;
+    struct sockaddr_in server;
+
+  public:
+    TCPClient();
+    bool setup(string address, int port);
+    bool Send(string data);
+    string receive(int size = 4096);
+    string read();
+    void exit();
+};
+
+#endif
