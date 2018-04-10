ff09454365db171efb1b8edb2c69cf370ec915e9
Henrik Truelsen <31506722+henriktrue@users.noreply.github.com>
2018-04-10 16:33:45 +0200

    ny igen
    opdateret

--- opgave4/klient/main.cpp
+++ opgave4/klient/main.cpp
@@ -5,58 +5,60 @@
  *      Author: henrik
  */
 
-#include <iostream>
-#include <signal.h>
-#include "TCPconnection.h"
+ #include <iostream>
+ #include <signal.h>
+ #include "connect.h"
 #include "control.h"
-
-using namespace std;
-
-TCPClient tcp;
-int target = 25; //default target temperature
-float temp = 26; //default temperature reading resulting in "OFF" state
-
-template <typename T> string tostr(const T& t) {
-    ostringstream os;
-    os << t;
-    return os.str();
-}
-
-void sig_exit(int s) {
-    tcp.exit();
-    exit(0);
-}
-
-int main(int argc, char *argv[]) {
-
-    /* Input error handling */
-    if (argc == 2 && string(argv[1]).length() == 2) {
-        target = toint(string(argv[1]));
-    } else {
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <signal.h>
+#include <syslog.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <pthread.h>
+#include <stdlib.h>
+#include <fstream>
+ 
+ using namespace std;
+ 
+connect tcp;
+ int target = 25; //default target temperature
+ float temp = 26; //default temperature reading resulting in "OFF" state
+ 
+@@ -51,27 +60,29 @@ int main(int argc, char *argv[]) {
+     if (argc == 2 && string(argv[1]).length() == 2) {
+         target = toint(string(argv[1]));
+     } else {
         print_help();
-        exit(0);
-    }
-    signal(SIGINT, sig_exit);
+         exit(0);
+     }
+     signal(SIGINT, sig_exit);
+ 
+ 	// ip is 0.0.0.0 to listen on all interfaces
+     tcp.setup("0.0.0.0", 1955); // ip and port
+     while (1) {
+        tcp.Send("GETTEMP"); // ask host for temperature
+         string rec = tcp.receive(); // calls recv() and does error handling  
 
-    tcp.setup("127.0.0.1", 1955); // <ip>, <port>
-    while (1) {
-        tcp.Send("GETTEMP"); // Always ask the host for the temperature
-        string rec = tcp.receive(); // calls recv() and does error handling
+         if (rec != "") { // If rec is empty, recieve again
+             cout << "Temperature is: " << rec << " degC" << endl;
+         
 
-        if (rec != "") { // If rec is empty, recieve again
-            cout << "Temperature is " << rec << " degC" << endl;
+             // Convert temperature string to float, so i can compared to the setpoint in heater()
+            float temp = strtof((rec).c_str(),0);
 
-            /* Convert temperature string to float, making it possible to
-             * compare it to the setpoint in heater() */
-            temp = stof(rec);
-
-            /* Instructs the host to turn on/off the heater by sending a
-             * "1" or a "0" */
-            tcp.Send(tostr(heater(temp, target)));
-        }
-        sleep(3); // Don't spam the output (temperature changes are slow)
-    }
-    return 0;
+             // Instructs the host to turn heater on or off
+             tcp.Send(tostr(heater(temp, target)));
+         }
+         sleep(3); // wait 3 seconds
+     }
+     return 0;
 }
-
 
