13b48d351af01f0053af2f91b1c73546ff033119
Henrik Truelsen <31506722+henriktrue@users.noreply.github.com>
2018-04-10 18:47:18 +0200

    fixet bugs
    fixet bugs

--- opgave4/klient/main.cpp
+++ opgave4/klient/main.cpp
@@ -7,8 +7,7 @@
 
  #include <iostream>
  #include <signal.h>
- #include "connect.h"
-#include "control.h"
+ #include "TCPClient.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -19,7 +18,6 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <netdb.h>
-#include <iostream>
 #include <string>
 #include <sstream>
 #include <pthread.h>
@@ -28,21 +26,46 @@
  
  using namespace std;
  
-connect tcp;
+ void print_help(void) {
+    cout << "you need to supply an input argument temperature (2 digits)" << endl;
+    cout << endl;
+}
+
+int heater(float temperature, int target) {
+    if (temperature < target) {
+        cout << "Heater ON" << endl;
+        return 1;
+    } else {
+        cout << "Heater OFF" << endl;
+        return 0;
+    }
+}
+
+int toint(std::string s) //The conversion function
+{
+    return atoi(s.c_str());
+}
+
+void sig_exit(int s) {
+    tcp.exit();
+    exit(0);
+}
+ 
+TCPClient tcp;
  int target = 25; //default target temperature
  float temp = 26; //default temperature reading resulting in "OFF" state
  
-@@ -51,27 +60,29 @@ int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
      if (argc == 2 && string(argv[1]).length() == 2) {
          target = toint(string(argv[1]));
      } else {
         print_help();
-         exit(0);
+        exit(0);
      }
      signal(SIGINT, sig_exit);
  
  	// ip is 0.0.0.0 to listen on all interfaces
-     tcp.setup("0.0.0.0", 1955); // ip and port
+     tcp.setup("127.0.0.1", 1955); // ip and port
      while (1) {
         tcp.Send("GETTEMP"); // ask host for temperature
          string rec = tcp.receive(); // calls recv() and does error handling  
@@ -50,7 +73,6 @@ connect tcp;
          if (rec != "") { // If rec is empty, recieve again
              cout << "Temperature is: " << rec << " degC" << endl;
          
-
              // Convert temperature string to float, so i can compared to the setpoint in heater()
             float temp = strtof((rec).c_str(),0);
 
@@ -61,4 +83,3 @@ connect tcp;
      }
      return 0;
 }
-
