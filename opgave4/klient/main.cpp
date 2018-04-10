6b28e96505ce390247da72ad711f6bdbbe0e3769
Henrik Truelsen <31506722+henriktrue@users.noreply.github.com>
2018-04-10 16:29:52 +0200

    fandt fejæ
    asd

--- opgave4/klient/main.cpp
+++ opgave4/klient/main.cpp
@@ -1,22 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 3, 2018
+ *      Author: henrik
+ */
+
 #include <iostream>
 #include <signal.h>
-#include "connect.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <signal.h>
-#include <syslog.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <netdb.h>
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <pthread.h>
-#include <stdlib.h>
-#include <fstream>
+#include "TCPconnection.h"
+#include "control.h"
 
 using namespace std;
 
@@ -35,28 +27,9 @@ void sig_exit(int s) {
     exit(0);
 }
 
-void print_help(void) {
-    cout << "you need to supply an input argument temperature (2 digits)" << endl;
-    cout << endl;
-}
-
-int heater(float temperature, int target) {
-    if (temperature < target) {
-        cout << "Heater ON" << endl;
-        return 1;
-    } else {
-        cout << "Heater OFF" << endl;
-        return 0;
-    }
-}
-
-int toint(std::string s) //The conversion function
-{
-    return atoi(s.c_str());
-}
-
 int main(int argc, char *argv[]) {
-	// in case no input argument was not valid
+
+    /* Input error handling */
     if (argc == 2 && string(argv[1]).length() == 2) {
         target = toint(string(argv[1]));
     } else {
@@ -65,22 +38,23 @@ int main(int argc, char *argv[]) {
     }
     signal(SIGINT, sig_exit);
 
-	// ip is 0.0.0.0 to listen on all interfaces
-    tcp.setup("0.0.0.0", 1955); // ip and port
+    tcp.setup("127.0.0.1", 1955); // <ip>, <port>
     while (1) {
-        tcp.Send("GETTEMP"); // ask host for temperature
+        tcp.Send("GETTEMP"); // Always ask the host for the temperature
         string rec = tcp.receive(); // calls recv() and does error handling
 
         if (rec != "") { // If rec is empty, recieve again
-            cout << "Temperature is: " << rec << " degC" << endl;
+            cout << "Temperature is " << rec << " degC" << endl;
 
-            // Convert temperature string to float, so i can compared to the setpoint in heater()
-            float temp = strtof((rec).c_str(),0);
+            /* Convert temperature string to float, making it possible to
+             * compare it to the setpoint in heater() */
+            temp = stof(rec);
 
-            // Instructs the host to turn heater on or off
+            /* Instructs the host to turn on/off the heater by sending a
+             * "1" or a "0" */
             tcp.Send(tostr(heater(temp, target)));
         }
-        sleep(3); // wait 3 seconds
+        sleep(3); // Don't spam the output (temperature changes are slow)
     }
     return 0;
 }
